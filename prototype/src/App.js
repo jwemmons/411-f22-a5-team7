b51677388435fa955fd96a981c65235efbeaf43a
Joseph Jang <josephjang11@gmail.com>
2022-11-11 15:41:15 -0500

    Prototype folder

---
+++ prototype/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Form from './components/form';
+import Navbar from './components/navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Form></Form>
+    </div>
+  );
+}
+
+export default App;
