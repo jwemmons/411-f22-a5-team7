9afeb667ced39dd82829679c00f456eb6ab4ac7b
dougmoy <douglashtmoy@gmail.com>
2022-12-06 14:51:24 -0500

    added my designs for the front end

--- moviematch/src/components/form.js
+++ moviematch/src/components/form.js
@@ -9,9 +9,9 @@ function Form() {
   useEffect(() => {
 
     var movieList = {}
-    
+
     for (var i = 0; i < movieData.length; i++) {
-      movieList[movieData[i].title] = [movieData[i].poster_path, movieData[i].rating, movieData[i].runtime]
+      movieList[movieData[i].title] = [movieData[i].poster_path, movieData[i].rating]
     }
     Setmovies(movieList)
 
@@ -26,58 +26,58 @@ function Form() {
     var movieService = document.getElementById("inputService").value;
 
 
-    fetch("http://127.0.0.1:4000/movies/get_by_genre",
+    fetch("http://127.0.0.1:5000/movies/get_by_genre",
       {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          "genre": movieGenre,
-          "runtime": movieLength,
-          "service": movieService
+          "genre": movieGenre
         })
       }).then((response) => response.json())
       .then((data) => {
         console.log(data.results)
         setMovieData(data.results)
-        
-      }).catch ((error) => {
-          console.log(error);
+
+      }).catch((error) => {
+        console.log(error);
       })
-      
-    }
+
+  }
 
   return (
     <div className="form-container">
       <form>
         <div className='form-content'>
-          <div className='formTitle'>Movie Preferences</div> 
+          <div className='formTitle'>Movie Preferences:</div>
           <div className='inputs'>
             <label>
               <input type="text" id='inputGenre' placeholder='Genre'></input>
-            </label>  
+            </label>
             <label>
-              <input type="text" id='inputLength' placeholder='Max Length'></input>
+              <input type="text" id='inputLength' placeholder='Length'></input>
             </label>
             <label>
               <input type="text" id='inputService' placeholder='Streaming Service'></input>
-            </label> 
-            <button className='searchMovie' onClick={(e) =>  fetchMovies(e)}>SEARCH</button>
-          </div> 
+            </label>
+
+          </div>
+          <div>
+            <button className='searchMovie' onClick={(e) => fetchMovies(e)}>Find your movies!</button>
+          </div>
         </div>
         {/* Create New Div element to display movies, parse through data stored in movieData */}
         <div className='movieGrid'>
-          {movieData && Object.entries(movies).map(([ key, value ], i) => 
-              <div key={i} className="movieOutput">
+          {movieData && Object.entries(movies).map(([key, value], i) =>
+            <div key={i} className="movieOutput">
+              <div>
                 <div>
-                  <div>
-                  <img src={value[0]} alt="movie link"/>
-                  </div>
-                  <div>{key}</div>
-                  <div>{value[2]} min</div>
-                  <div>{value[1]}</div>
+                  <img src={value[0]} alt="movie link" />
                 </div>
+                <div>{key}</div>
+                <div>{value[1]}</div>
               </div>
-            )
+            </div>
+          )
           }
         </div>
       </form>
--- moviematch/src/components/navbar.js
+++ moviematch/src/components/navbar.js
@@ -3,18 +3,18 @@ import '../styles/navbar.css'
 function Navbar() {
   return (
     <div className="navbar-container">
-        <div className='navbar-content'>
+      <div className='navbar-content'>
 
-            <div className='appTitle'>
-                    Movie Match
-            </div>
-            <div>
-                <button className='loginButton'>Login</button>
-            </div>
-            <hr></hr>
+        <div className='appTitle'>
+          MovieMatch
         </div>
-      
-      
+        <div>
+          <button className='loginButton'>Login</button>
+        </div>
+        <hr></hr>
+      </div>
+
+
     </div>
   );
 }
