7202147b943776d0433762568df8b26b7f5ba164
Dhruv Chandwani <dhruvchandwani23@gmail.com>
2022-12-08 01:38:11 -0500

    User added to db on login, fixed issue with spotipy caching

--- moviematch/backend/app.py
+++ moviematch/backend/app.py
@@ -24,6 +24,7 @@ db.init_app(app)
 
 app.config['SECRET_KEY'] = "secret_key"
 app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_PERMANENT'] = 'False'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
 Session(app)
 
@@ -38,17 +39,21 @@ def login():
     auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-email ', cache_handler=cache_handler,
                                                show_dialog=True, client_id=config.CLIENT_ID, client_secret=config.CLIENT_SECRET, redirect_uri=config.REDIRECT_URI)
 
-    if request.args.get('code'):
-        print('code is in request.args')
-        auth_manager.get_access_token(request.args.get('code'))
-        return redirect('/login')
+    # if request.args.get('code'):
+    #     print('code is in request.args')
+    #     auth_manager.get_access_token(request.args.get('code'))
+    #     return redirect('/login')
 
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
         print('no valid token, need to sign-in with spotify')
         auth_url = auth_manager.get_authorize_url()
         #return f'<a href="{auth_url}">Sign in with Spotify</a>'
         print("got here")
+
         return jsonify({"url": auth_url})
+    else:
+        return redirect("http://localhost:3000/search")
+
         # return redirect(auth_url)
     # else:
     #     print('signed in successfully')
@@ -64,24 +69,37 @@ def login():
 def callback():
     code = request.args.get('code')
     session['code'] = code
-    # new_url = '/login?code=' + code
-    new_url = "http://localhost:3000/search"
-    return redirect(new_url)
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-email ', cache_handler=cache_handler,\
+                                               show_dialog=True, client_id=config.CLIENT_ID, client_secret=config.CLIENT_SECRET, redirect_uri=config.REDIRECT_URI)
+    auth_manager.get_access_token(code)
+
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+    me = sp.me()
+    if not(UserModel.query.filter_by(user_id=me['id']).first()):
+        user = UserModel(me['id'],me['display_name'],"{}")
+        db.session.add(user)
+        db.session.commit()
+    url = "http://localhost:3000/search"
+    print(session)
+    return redirect(url)
 
 @app.route('/logout')
 def logout():
-    session.pop("token_info", None)
+    session.clear()
+    print(session)
     return jsonify({'url': 'http://localhost:3000/home'})
 
 @app.route('/user/add_fav/<movie_id>')
 def add_fav(movie_id):
-    # cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
-    # auth_manager = spotipy.oauth2.SpotifyOAuth(
-    #         cache_handler=cache_handler, client_id=config.CLIENT_ID, client_secret=config.CLIENT_SECRET, redirect_uri=config.REDIRECT_URI)
-    # if not auth_manager.validate_token(cache_handler.get_cached_token()):
-    #         return redirect('/login')
-    # sp = spotipy.Spotify(auth_manager=auth_manager)
-    # user_id = sp.me()['id']
-    # data = UserModel.query.filter_by(user_id=user_id).first()
-    pass
+    print(session)
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(
+            cache_handler=cache_handler, client_id=config.CLIENT_ID, client_secret=config.CLIENT_SECRET, redirect_uri=config.REDIRECT_URI)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+            return redirect('/login')
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+    user_id = sp.me()['display_name']
+    data = UserModel.query.filter_by(user_id=user_id).first()
+    return {"data": user_id}
     
